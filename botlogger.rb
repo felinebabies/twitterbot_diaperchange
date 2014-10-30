9a9e8adcb80acbc4ff3d4236ecf584d194d3d9ee
syuji <commentonly@livedoor.com>
2014-10-31 01:41:46 +0900

    ログ管理用モジュールを追加した。

---
+++ botlogger.rb
@@ -0,0 +1,18 @@
+﻿# coding: utf-8
+#ログ設定モジュール
+
+require 'logger'
+
+module BotLogger
+  @log_dir = File.join( File.dirname(__FILE__), 'log')
+  FileUtils.mkdir(@log_dir)  if !File.exists?(@log_dir)
+
+  @log = Logger.new("log/botlog.txt", 100, 10 * 1024 * 1024)
+
+  # FATAL, ERROR, WARN, INFO, DEBUG
+  @log.level = Logger::INFO
+
+  def self.log
+    return @log
+  end
+end
